cb3cab0fbe0fda4ca3cb263841081e27d5225107
june lee <melodysdreamj@gmail.com>
2024-06-02 20:47:45 +0900

    Initial commit

---
+++ lib/util/_/build_app/widget/material_app/listener_lego/_.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import '../../../../../../main.dart';
+
+// @CoverMaterialApp()
+// Widget coverListenerLego(BuildContext context, Widget widget) {
+//   return widget;
+// }
---
+++ lib/util/_/build_app/widget/my_app/listener_lego/_.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import '../../../../../../main.dart';
+
+// @CoverMyApp()
+// Widget Function() coverListenerLego(Widget Function() materialAppBuilder) {
+//   return () => materialAppBuilder();
+// }
